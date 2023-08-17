5321d98b2e599af03377072bffbd20d20286e988
Hyeong-Uk <kohowo1999@snu.ac.kr>
2023-08-18 03:51:32 +0900

    hk7

--- database.py
+++ database.py
@@ -15,7 +15,8 @@ pseudoslimestat_n = stat_n(hp = 10.0, atk = 2, def_ = 0, hitrate = 100.0, dodger
 # weapons
 baseballbatstat_n = stat_n(hp = 0.0, atk = 3, def_ = 0, hitrate = 0.0, dodgerate = 0.0,
                   critrate = 5.0, critres = 0.0, critdmg = 0.0, guardrate = 0.0, guarddmgdec = 0.0, brkthr = 0.0, speed = 0)
-baseballbat = Weapon(name = "야구배트", type_= "전설의 무기", element = "None", stat_n = baseballbatstat_n, stat_s = stat_s())
+baseballbatexplain = "전설의 대장장이 HK가 만들었다고 전해지는? 야구배트이다. 몬스터를 팰 때 매우 유용하다. 그 단단함은 이루 말할 수 없다. 다만, 본래 용도와 다르게 사용하면 무슨 일이 일어날지도 모른다.."
+baseballbat = Weapon(name = "야구배트", type_= "전설의 무기", explanation = baseballbatexplain, element = "None", stat_n = baseballbatstat_n, stat_s = stat_s())
 # armors
 # accesories
 
