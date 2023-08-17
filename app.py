65fe91bcfc1b94ec9c8ab096afcda05868e2f9fe
kakunge <kakunge3@naver.com>
2023-08-17 20:34:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+@app.route('/index')
+def print_index():
     return render_template("index.html")
 
 @app.route('/1')
-def goto1():
+def print_1():
     return render_template("1.html")
 
 if __name__ == "__main__":
