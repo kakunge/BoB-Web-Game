3defd4f314f617280aef7ac5607fb1db3f5a8910
moon seoung hyun <msh070809@naver.com>
2023-08-17 22:32:07 +0900

    sign up &&db connect /2

--- app.py
+++ app.py
@@ -2,32 +2,35 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/dbname'  # MySQL 연결 정보 입력
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://msh070809:pwpw@localhost:3306/Account'
 db = SQLAlchemy(app)
 
-# 모델 클래스 임포트
 from model import User
 
 @app.route('/')
 def login():
     return render_template('login.html')
 
-@app.route('/report', methods=['POST'])
+@app.route('/report', methods=['GET', 'POST'])
 def report():
-    userid = request.form['userid']
-    userpw = request.form['userpw']
+    if request.method == 'POST':
+        userid = request.form['userid']
+        userpw = request.form['userpw']
 
-    lower_letter = any(c.islower() for c in userpw)
-    upper_letter = any(c.isupper() for c in userpw)
-    num_end = userid[-1].isdigit()
-    report = lower_letter & upper_letter & num_end
+        lower_letter = any(c.islower() for c in userpw)
+        upper_letter = any(c.isupper() for c in userpw)
+        num_end = userid[-1].isdigit()
+        report = lower_letter & upper_letter & num_end
 
-    if report:
-        new_user = User(username=userid, userpw=userpw)
-        db.session.add(new_user)
-        db.session.commit()
+        if report:
+            new_user = User(username=userid, userpw=userpw)
+            db.session.add(new_user)
+            db.session.commit()
 
-    return render_template('report.html', userid=userid, userpw=userpw, lower=lower_letter, upper=upper_letter, num_end=num_end, report=report)
+        return render_template('report.html', userid=userid, userpw=userpw, lower=lower_letter, upper=upper_letter, num_end=num_end, report=report)
+    
+    # GET 요청일 경우, 폼을 보여줍니다.
+    return render_template('report.html', userid='', userpw='', lower=False, upper=False, num_end=False, report=False)
 
 if __name__ == '__main__':
     app.run(debug=True)
