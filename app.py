27df27c81091ac9b7bd345ccbb91467753162426
moon seoung hyun <msh070809@naver.com>
2023-08-18 00:36:46 +0900

    login /sign up /1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session, url_for
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://msh070809:pwpw@localhost:3306/Account'
@@ -10,7 +11,36 @@ db = SQLAlchemy(app)
 def login():
     return render_template('login.html')
 
-@app.route('/report', methods=['GET', 'POST'])
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)  # 세션에서 사용자 정보 제거
+    return redirect(url_for('index'))
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def login():
+    return render_template('signup.html')
+
+#id가 틀렸는지 pw가 틀렸는지 알려줘야 하지만 시간 관계상 생략
+#로그인 검사 db에 데이터가 있으면 세션 추가 후 login화면 전환 ,없을경우 something출력
+@app.route('/login_check', methods=['GET', 'POST'])
+def report():
+    if request.method == 'POST':
+        from model import User
+        userid  = request.form['userid']
+        userpw = request.form['userpw']
+
+        result = User.query.fillter_by(userid=userid).first()
+        if result and check_password_hash(result.userpw, userpw):
+            session['user_id'] = result.userid
+            return render_template('login.html')
+    return "something, problem"
+
+
+@app.route('/judge_signup', methods=['GET', 'POST'])
 def report():
     if request.method == 'POST':
         from model import User
@@ -23,14 +53,15 @@ def report():
         report = lower_letter & upper_letter & num_end
 
         if report:
-            new_user = User(username=userid, userpw=userpw)
+            new_user = User(userid=userid, userpw=userpw)
             db.session.add(new_user)
             db.session.commit()
 
-        return render_template('report.html', userid=userid, userpw=userpw, lower=lower_letter, upper=upper_letter, num_end=num_end, report=report)
+        return render_template('signup_judge.html', userid=userid, userpw=userpw, lower=lower_letter, upper=upper_letter, num_end=num_end, report=report)
     
     # GET 요청일 경우, 폼을 보여줍니다.
-    return render_template('report.html', userid='', userpw='', lower=False, upper=False, num_end=False, report=False)
+    return render_template('signup_judge.html', userid='', userpw='', lower=False, upper=False, num_end=False, report=False)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- model.py
+++ model.py
@@ -2,5 +2,5 @@ from app import db
 
 class User(db.Model):
     __tablename__ = "USER"
-    username = db.Column("userid",db.String(80), unique=True, nullable=False,primary_key=True)
+    userid = db.Column("userid",db.String(80), unique=True, nullable=False,primary_key=True)
     userpw = db.Column("userpw",db.String(120), nullable=False)
