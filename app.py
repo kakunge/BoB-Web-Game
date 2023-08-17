98972055419acd02941074a178df39550ff473c1
moon seoung hyun <msh070809@naver.com>
2023-08-18 04:26:44 +0900

    mod .html

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ from database import *
 app = Flask(__name__)
 app.logger.setLevel(logging.DEBUG)
 app.secret_key = 'kldjfkdufuk'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://msh070809:pwpw@localhost:3306/Account'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://msh070809:pwpw@localhost:3306/Game'
 db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__ = "USER"
+    __tablename__ = "User"
     userid = db.Column("userid",db.String(80), unique=True, nullable=False,primary_key=True)
     userpw = db.Column("userpw",db.String(120), nullable=False)
 
@@ -21,11 +21,17 @@ player = None
 monster = None
 
 @app.route('/')
-@app.route('/index')
-    # return render_template("index.html")
-
 def login():
     return render_template('login.html')
+
+@app.route('/index')
+def index():
+    if 'user_id' in session:
+        # 세션에 user_id가 있으면 로그인 상태로 간주하여 로그인 이후의 동작을 수행합니다.
+        user_id = session['user_id']
+        return f'로그인된 사용자: {user_id}'
+    # 세션에 user_id가 없으면 비로그인 상태로 간주하여 로그인 이전의 동작을 수행합니다.
+    return '로그인되지 않은 상태'
 
 @app.route('/logout')
 def logout():
@@ -73,7 +79,6 @@ def judge_signup():
             app.logger.warning("Signup failed")
             return render_template('signup_judge.html', userid=userid, userpw=userpw, lower=lower_letter, upper=upper_letter, num_end=num_end, report=report)
 
-# ...
 
 if __name__ == '__main__':
     db.init_app(app)
